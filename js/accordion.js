e46fa3dbcdf1f2ea0376b6c3db05f90cbd0c8214
Tom Tillistrand <tomtillistrand@gmail.com>
2021-02-09 14:56:22 -0500

    Refactored with Panel class

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,3 @@
-import animate from './animate.js';
 import Panel from './panel.js';
 
 class Accordion {
@@ -7,109 +6,48 @@ class Accordion {
       throw new Error('No element passed into Accordion');
     }
 
-    this.accordion = el;
-    this.accordionId = Accordion.accordionCount;
-    this.accordion.setAttribute('role', 'tablist');
-    this.panels = el.querySelectorAll('.accordion__panel');
-    [this.activePanel] = this.panels;
-    this.state = {
-      isAnimating: false,
-    };
-
-    this.panels.forEach((panel, i) => {
-      const isOpen = i === 0;
-      const panelHeader = panel.querySelector('.accordion__header');
-      const panelContent = panel.querySelector('.accordion__panel-content');
-      const panelHeaderId = `acc-${this.accordionId}-header-${i}`;
-      const panelContentId = `acc-${this.accordionId}-content-${i}`;
-
-      panelHeader.id = panelHeaderId;
-      panelHeader.setAttribute('role', 'tab');
-      panelHeader.setAttribute('aria-controls', panelContentId);
-      panelHeader.setAttribute('aria-selected', isOpen);
-      panelHeader.setAttribute('aria-expanded', isOpen);
-      panelHeader.tabIndex = '0';
-
-      panelContent.id = panelContentId;
-      panelContent.setAttribute('role', 'tabpanel');
-      panelContent.hidden = !isOpen;
-      panelContent.setAttribute('aria-labelledby', panelHeaderId);
-      panelContent.style.opacity = isOpen ? '1' : '0';
-      panelContent.style.height = isOpen
-        ? `${panelContent.scrollHeight}`
-        : `0px`;
-
-      panelHeader.addEventListener('click', () => {
-        if (panel === this.activePanel || this.state.isAnimating) return;
-        this.state.isAnimating = true;
-        this.togglePanel(this.activePanel); // close active panel
-        this.togglePanel(panel); // open clicked panel
-        this.activePanel = panel;
-      });
-
-      panelHeader.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-          if (panel === this.activePanel || this.state.isAnimating) return;
-          this.state.isAnimating = true;
-          this.togglePanel(this.activePanel); // close active panel
-          this.togglePanel(panel); // open clicked panel
-          this.activePanel = panel;
-        }
-      });
-
-      panelHeader.addEventListener('mousedown', (e) => {
-        e.preventDefault();
-      });
-    });
+    this.domEl = el;
+    this.id = Accordion.accordionCount;
+    this.domEl.setAttribute('role', 'tablist');
+    this.togglePanels = this.togglePanels.bind(this);
+    this.panels = Array.from(
+      this.domEl.querySelectorAll('.accordion__panel')
+    ).map(
+      (domPanel, i) => new Panel(domPanel, i, this, this.id, this.togglePanels)
+    );
+    this._isAnimating = false;
+    this.activePanelId = this.panels[0].panelId;
 
     Accordion.accordionCount += 1;
   }
 
-  static accordionCount = 0; // Used to create unique ids for each accordion
+  get isAnimating() {
+    return this._isAnimating;
+  }
 
-  togglePanel(panel) {
-    const animationSpeed = 200; // milliseconds
-    const panelHeader = panel.querySelector('.accordion__header');
-    const panelContent = panel.querySelector('.accordion__panel-content');
+  set isAnimating(isAnimating) {
+    this._isAnimating = isAnimating;
+  }
 
-    const isOpen = !(panelHeader.getAttribute('aria-selected') === 'true');
-    if (isOpen) panelContent.removeAttribute('hidden');
-    const fullHeight = panelContent.scrollHeight;
-    panelHeader.setAttribute('aria-selected', isOpen);
-    panelHeader.setAttribute('aria-expanded', isOpen);
+  togglePanels(event, activatedPanelId) {
+    if (this.isAnimating) return;
 
-    if (isOpen) {
-      // Animate panel open
-      animate(function draw(progress) {
-        const newHeight = progress * fullHeight;
-        const newOpacity = progress * 1;
-        panelContent.style.height = `${newHeight}px`;
-        panelContent.style.opacity = `${newOpacity}`;
-      }, animationSpeed)
-        .then(() => {
-          this.state.isAnimating = false;
-          panelContent.style.height = 'auto';
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    } else {
-      // Animate panel closed
-      animate(function draw(progress) {
-        const newHeight = (1 - progress) * fullHeight;
-        const newOpacity = (1 - progress) * 1;
-        panelContent.style.height = `${newHeight}px`;
-        panelContent.style.opacity = `${newOpacity}`;
-      }, animationSpeed)
-        .then(() => {
-          this.state.isAnimating = false;
-          panelContent.hidden = true;
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+    if (
+      event.type === 'click' ||
+      (event.type === 'keydown' && event.key === 'Enter')
+    ) {
+      this.isAnimating = true;
+      this.panels
+        .find((panel) => panel.panelId === this.activePanelId)
+        .togglePanel();
+      this.panels
+        .find((panel) => panel.panelId === activatedPanelId)
+        .togglePanel();
+      this.activePanelId = activatedPanelId;
     }
   }
+
+  static accordionCount = 0; // Used to create unique ids for each accordion
 }
 
 export default Accordion;
--- js/panel.js
+++ js/panel.js
@@ -1,5 +1,91 @@
+import animate from './animate.js';
+
 class Panel {
-  constructor(el) {}
+  constructor(panelEl, index, accordion) {
+    this.domEl = panelEl;
+    this.parentAccordion = accordion;
+    this._isOpen = index === 0; // First panel is active
+    this.panelId = `acc-${accordion.id}-panel-${index}`;
+    this.panelContentId = `acc-${accordion.id}-content-${index}`;
+    this.panelHeaderId = `acc-${accordion.id}-header-${index}`;
+
+    this.panelHeader = this.domEl.querySelector('.accordion__header');
+    this.panelHeader.id = this.panelHeaderId;
+    this.panelHeader.setAttribute('role', 'tab');
+    this.panelHeader.setAttribute('aria-controls', this.panelContentId);
+    this.panelHeader.tabIndex = '0';
+
+    this.panelContent = this.domEl.querySelector('.accordion__panel-content');
+    this.panelContent.id = this.panelContentId;
+    this.panelContent.setAttribute('role', 'tabpanel');
+    this.panelContent.setAttribute('aria-labelledby', this.panelHeaderId);
+    this.panelContent.hidden = !this.isOpen;
+    this.panelContent.style.opacity = this.isOpen ? '1' : '0';
+    this.panelContent.style.height = this.isOpen
+      ? `${this.panelContent.scrollHeight}`
+      : `0px`;
+
+    this.handleActivate = this.handleActivate.bind(this);
+
+    this.panelHeader.addEventListener('click', this.handleActivate);
+
+    this.panelHeader.addEventListener('keydown', this.handleActivate);
+
+    this.panelHeader.addEventListener('mousedown', (e) => {
+      e.preventDefault();
+    });
+  }
+
+  handleActivate(event) {
+    if (this.isOpen) return;
+    console.log(this.parentAccordion);
+    this.parentAccordion.togglePanels(event, this.panelId);
+  }
+
+  async animate(isOpening) {
+    const animationSpeed = 200; // milliseconds
+    const fullHeight = this.panelContent.scrollHeight;
+
+    await animate((progress) => {
+      let newHeight;
+      let newOpacity;
+      if (isOpening) {
+        newHeight = progress * fullHeight;
+        newOpacity = progress * 1;
+      } else {
+        newHeight = (1 - progress) * fullHeight;
+        newOpacity = (1 - progress) * 1;
+      }
+
+      this.panelContent.style.height = `${newHeight}px`;
+      this.panelContent.style.opacity = `${newOpacity}`;
+    }, animationSpeed);
+
+    this.parentAccordion.isAnimating = false;
+    if (isOpening) {
+      this.panelContent.style.height = 'auto';
+    } else {
+      this.panelContent.hidden = true;
+    }
+  }
+
+  togglePanel() {
+    this.isOpen = !this.isOpen;
+    this.animate(this.isOpen).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  get isOpen() {
+    return this._isOpen;
+  }
+
+  set isOpen(isOpen) {
+    this.panelHeader.setAttribute('aria-selected', isOpen);
+    this.panelHeader.setAttribute('aria-expanded', isOpen);
+    if (isOpen) this.panelContent.removeAttribute('hidden');
+    this._isOpen = isOpen;
+  }
 }
 
 export default Panel;
